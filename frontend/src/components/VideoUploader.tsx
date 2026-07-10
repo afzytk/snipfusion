b4b35cc8d16136190e389b11b420e7433b93c998
afzytk <afzytk@gmail.com>
2026-07-10 17:00:25 +0530

    feat: add state logic to capture video file

--- frontend/src/components/VideoUploader.tsx
+++ frontend/src/components/VideoUploader.tsx
@@ -1,6 +1,15 @@
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const VideoUploader = () => {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setVideoFile(file);
+      console.log("File captured successfully:", file.name);
+    }
+  };
   return (
     <div className="flex flex-col gap-4 justify-center m-4 items-center">
       <h1 className="text-2xl color">Upload a video</h1>
@@ -9,6 +18,7 @@ const VideoUploader = () => {
         type="file"
         accept="video"
         className="cursor-pointer hover:border-blue-400"
+        onChange={handleFileChange}
       />
     </div>
   );
